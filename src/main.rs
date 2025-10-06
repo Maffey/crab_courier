4928b8c464ecaca415974a99df6e1ca78a4e399e
Mateusz Kuniczuk <mateuszkuniczuk98@gmail.com>
2025-10-06 23:09:39 +0200

    todo for future

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use dotenvy::dotenv;
 use crab_courier::{Configuration, run};
 
 fn main() {
+    // TODO tests
     dotenv().ok();
     let configuration = Configuration::new();
     run(configuration);
