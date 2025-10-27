2e4a59d4df1335a19c1bfe4a0566e4e5a3e29a0d
Mateusz Kuniczuk <mateuszkuniczuk98@gmail.com>
2025-10-27 23:34:13 +0100

    secrets through cli
    Allow interchangeably to use env vars or CLI args. Clean up old struct and implementation, unify into single lib file.

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,10 @@ use lettre::message::{Attachment, MultiPart, SinglePart};
 use lettre::transport::smtp::authentication::Credentials;
 use lettre::{Message, SmtpTransport, Transport};
 use std::path::Path;
-use std::{env, fs};
+use std::fs;
+use clap::Parser;
 
 const APP_SHORT_NAME: &str = "c_c";
-const GMAIL_USERNAME: &str = "GMAIL_USER";
-const GMAIL_APPLICATION_PASSWORD: &str = "GMAIL_APPLICATION_PASSWORD";
-const EMAIL_RECIPIENT: &str = "EMAIL_RECIPIENT";
 
 const EMAIL_MESSAGE: &str = r#"
 The requested book is attached to this email.
@@ -17,17 +15,17 @@ The requested book is attached to this email.
 Delivered by c_c | crab_courier
 "#;
 
-pub fn run(env_variables: Secrets, path_to_ebook: &str) -> Result<()> {
-    let email = build_email(&env_variables, path_to_ebook).context("Failed to build the email")?;
-    let mailer = get_gmail_mailer(&env_variables);
+pub fn run(arguments: &Arguments) -> Result<()> {
+    let email = build_email(&arguments).context("Failed to build the email")?;
+    let mailer = get_gmail_mailer(&arguments);
 
     send_email(mailer, email).context("Failed to send the email")?;
 
     Ok(())
 }
 
-fn build_email(env_variables: &Secrets, path_to_ebook: &str) -> Result<Message> {
-    let path_to_ebook = Path::new(path_to_ebook);
+fn build_email(arguments: &Arguments) -> Result<Message> {
+    let path_to_ebook = Path::new(&arguments.path_to_ebook);
     let filename = path_to_ebook
         .file_name()
         .context("Provided path has no valid file name")?
@@ -37,13 +35,13 @@ fn build_email(env_variables: &Secrets, path_to_ebook: &str) -> Result<Message> 
 
     Message::builder()
         .from(
-            env_variables
+            arguments
                 .username
                 .parse()
                 .context("Failed to parse username.")?,
         )
-        .to(env_variables
-            .email_recipient
+        .to(arguments
+            .recipient
             .parse()
             .context("Failed to parse email recipient")?)
         .subject(format!("{APP_SHORT_NAME} | {filename}"))
@@ -73,10 +71,10 @@ fn get_text_part() -> SinglePart {
         .body(String::from(EMAIL_MESSAGE))
 }
 
-fn get_gmail_mailer(env_variables: &Secrets) -> SmtpTransport {
+fn get_gmail_mailer(arguments: &Arguments) -> SmtpTransport {
     let credentials = Credentials::new(
-        env_variables.username.clone(),
-        env_variables.app_password.clone(),
+        arguments.username.clone(),
+        arguments.password.clone(),
     );
 
     SmtpTransport::relay("smtp.gmail.com")
@@ -90,38 +88,13 @@ fn send_email(mailer: SmtpTransport, email: Message) -> Result<()> {
     Ok(())
 }
 
-pub struct Secrets {
-    username: String,
-    app_password: String,
-    email_recipient: String,
-}
-
-impl Secrets {
-    pub fn new() -> Self {
-        Secrets {
-            username: env::var(GMAIL_USERNAME)
-                .expect("GMAIL_USERNAME must be set in the environment."),
-            app_password: env::var(GMAIL_APPLICATION_PASSWORD)
-                .expect("GMAIL_APPLICATION_PASSWORD must be set in the environment."),
-            email_recipient: env::var(EMAIL_RECIPIENT)
-                .expect("EMAIL_RECIPIENT must be set in the environment."),
-        }
-    }
-}
-
-impl Default for Secrets {
-    fn default() -> Self {
-        Secrets::new()
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::fs::File;
     use std::io::Write;
     use std::path::PathBuf;
-    use tempfile::{TempDir, tempdir};
+    use tempfile::{tempdir, TempDir};
 
     #[test]
     fn test_get_text_part_content() {
@@ -136,7 +109,7 @@ mod tests {
     fn test_get_attachment() {
         let fake_ebook_name = "fake_ebook.epub";
         let file_content = "This is a pretty useless book.";
-        // Need to get temp_dir so it, and created file, exists in the scope until end of the test
+        // Need to get temp_dir so it, and created file, exist in the scope until end of the test
         let (_temp_dir, file_path) = create_temp_file(fake_ebook_name, file_content);
 
         let attachment = get_attachment(&file_path, fake_ebook_name.to_string().clone());
@@ -152,4 +125,30 @@ mod tests {
         writeln!(file, "{}", content).expect("Failed to write to temporary file");
         (dir, file_path)
     }
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Arguments {
+    /// The path to ebook file to send to the recipient.
+    path_to_ebook: String,
+
+    /// The Gmail username to send email from.
+    /// Can also be set using the environment variable.
+    #[arg(long, short, env = "GMAIL_USERNAME")]
+    username: String,
+
+    /// The Gmail application password.
+    /// Can also be set using the environment variable.
+    #[arg(long, short, env = "GMAIL_APPLICATION_PASSWORD")]
+    password: String,
+
+    /// The email address of the recipient.
+    /// Can also be set using the environment variable.
+    #[arg(long, short, env = "EMAIL_RECIPIENT")]
+    recipient: String,
+}
+
+pub fn get_arguments() -> Arguments {
+    Arguments::parse()
 }
